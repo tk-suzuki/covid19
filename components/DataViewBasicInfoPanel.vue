64655e8208c8083854767698fa0353caa45173cc
mame3619 <mame3619@gmail.com>
2020-03-19 17:13:53 +0900

    各カードのデザインの大幅修正

--- components/DataViewBasicInfoPanel.vue
+++ components/DataViewBasicInfoPanel.vue
@@ -10,63 +10,49 @@
 </template>
 
 <style lang="scss">
-.DataView {
-  &-DataInfo {
-    margin-top: 20px;
-    text-align: right;
-    &-summary {
-      display: inline-block;
-      font-family: Hiragino Sans;
-      font-style: normal;
-      font-size: 30px;
-      line-height: 30px;
-      &-unit {
-        font-size: 0.6em;
+  .DataView {
+    &-DataInfo {
+      @include largerThan($large) {
+        text-align: right;
+        width: 50%;
+      }
+      &-summary {
+        display: inline-block;
+        font-family: Hiragino Sans;
+        font-style: normal;
+        font-size: 30px;
+        line-height: 30px;
+        &-unit {
+          font-size: 0.6em;
+        }
+      }
+      &-date {
+        white-space: normal;
+        display: inline-block;
+        font-size: 12px;
+        line-height: 12px;
+        color: $gray-3;
       }
     }
-    &-date {
-      white-space: nowrap;
-      display: inline-block;
-      font-size: 12px;
-      line-height: 12px;
-      color: $gray-3;
-    }
   }
-}
-.DataView {
-  @include card-container();
-  height: 100%;
-  &-Header {
-    background-color: transparent !important;
-    height: auto !important;
-  }
-  &-TitleContainer {
-    padding: 14px 0 8px;
-  }
-  &-Title {
-    @include card-h2();
-  }
-  &-Footer {
-    background-color: $white !important;
-    text-align: right;
-    margin: 2px 4px 12px;
-    flex-direction: row-reverse;
-    @include font-size(12);
-    color: $gray-3 !important;
-  }
-}
-.v-toolbar__content {
-  height: auto !important;
-}
 </style>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator'
-
-@Component
-export default class DataViewBasicInfoPanel extends Vue {
-  @Prop() private lText!: string
-  @Prop() private sText!: string
-  @Prop() private unit!: string
-}
+  import Vue from 'vue'
+  export default Vue.extend({
+    props: {
+      lText: {
+        type: String,
+        required: true
+      },
+      sText: {
+        type: String,
+        required: true
+      },
+      unit: {
+        type: String,
+        required: true
+      }
+    }
+  })
 </script>
