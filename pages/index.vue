d4e323ec59adf72b9fab6deb8b8eb29aedc81c9f
Nekoya3 <taiyu31@gmail.com>
2020-03-20 21:39:24 +0900

    一部の翻訳漏れを修正

--- pages/index.vue
+++ pages/index.vue
@@ -293,9 +293,16 @@ export default {
           row['居住地'] = this.$t(row['居住地'])
           row['性別'] = this.$t(row['性別'])
           row['退院'] = this.$t(row['退院'])
+          if (row['性別'] === '非公表性'){
+            row['性別'] = this.$t('非公表')
+          }
           if (row['年代'] === '10歳未満') {
             row['年代'] = this.$t('10歳未満')
-          } else {
+          }
+          else if (row['年代'] === '非公表'){
+            row['年代'] = this.$t('非公表')
+          }
+          else {
             const age = row['年代'].substring(0, 2)
             row['年代'] = this.$t('{age}代', { age })
           }
