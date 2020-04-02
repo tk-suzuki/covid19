c73d39f02f5c55f51921dd5c40d4f7844f95679d
Nekoya3 <taiyu31@gmail.com>
2020-04-02 15:01:35 +0900

    generate設定修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -207,8 +207,37 @@ module.exports = {
     "start_url": "/",
     "splash_pages": null
   },
+
   generate: {
-    fallback: true
+    fallback: true,
+    routes() {
+      const locales = ['ja', 'en', 'zh-cn', 'zh-tw', 'ko', 'ja-basic']
+      const pages = [
+        '/cards/details-of-confirmed-cases',
+        '/cards/details-of-tested-cases',
+        '/cards/number-of-confirmed-cases',
+        '/cards/attributes-of-confirmed-cases',
+        '/cards/number-of-tested',
+        '/cards/number-of-inspection-persons',
+        '/cards/number-of-reports-to-covid19-telephone-advisory-center',
+        '/cards/number-of-reports-to-covid19-consultation-desk',
+        '/cards/predicted-number-of-toei-subway-passengers',
+        '/cards/agency'
+      ]
+
+      const routes = []
+      locales.forEach(locale => {
+        pages.forEach(page => {
+          if (locale === 'ja') {
+            routes.push(page)
+            return
+          }
+          const route = `/${locale}${page}`
+          routes.push(route)
+        })
+      })
+      return routes
+    }
   },
   // /*
   // ** hot read configuration for docker
