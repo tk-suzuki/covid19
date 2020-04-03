a7e4d0867a372f36462833f9c34d9caff2280aa5
Nekoya3 <taiyu31@gmail.com>
2020-04-03 11:04:50 +0900

    OGP生成周りの修正

--- ui-test/ogp-screenshot.py
+++ ui-test/ogp-screenshot.py
@@ -8,13 +8,13 @@ if not os.path.exists("ogp"):
 
 PATHS = {
   "/?dummy": (959, 500),
-  "/cards/current-patients": (959, 500),
-  "/cards/contacts": (959, 500),
-  "/cards/discharges-summary": (959, 480),
-  "/cards/inspections": (959, 540),
-  "/cards/patients": (959, 500),
-  "/cards/patients-summary": (959, 500),
-  "/cards/querents": (959, 750)
+  "/cards/current-patients": (959, 900),
+  "/cards/contacts": (959, 900),
+  "/cards/discharges-summary": (959, 900),
+  "/cards/inspections": (959, 900),
+  "/cards/patients": (959, 900),
+  "/cards/patients-summary": (959, 900),
+  "/cards/querents": (959, 950)
 }
 
 options = webdriver.ChromeOptions()
