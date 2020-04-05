faee5c5631362eb77b6ff567ea6e71926eacf463
Nekoya3 <taiyu31@gmail.com>
2020-04-05 13:54:49 +0900

    OGPの生成をscrapingへ移動

--- pages/cards/_card.vue
+++ pages/cards/_card.vue
@@ -84,12 +84,12 @@ export default {
     }
   },
   head() {
-    const url = 'https://stopcovid19-dev.hokkaido.dev/'
+    const url = 'https://stopcovid19.hokkaido.dev/'
     const timestamp = new Date().getTime()
     const ogpImage =
       this.$i18n.locale === 'ja'
-        ? `${url}/ogp/${this.$route.params.card}.png?t=${timestamp}`
-        : `${url}/ogp/${this.$i18n.locale}/${this.$route.params.card}.png?t=${timestamp}`
+        ? `${url}/api/${this.$route.params.card}.png?t=${timestamp}`
+        : `${url}/api/${this.$i18n.locale}/${this.$route.params.card}.png?t=${timestamp}`
     const description = `${this.$t(
       '当サイトは、道内の新型コロナウイルス感染症（COVID-19）に関する最新情報を提供するために作成されました。開発は、ICTエンジニアやデザイナーなどによって結成された「JUST道IT」が行っています。'
     )}`
